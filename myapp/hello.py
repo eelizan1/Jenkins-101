102b159376e0e437b7458e29704e4a6921198bb6
Enrico Elizan <Enrico.Elizan@Siriusxm.com>
2023-04-27 11:57:52 -0400

    Updated jenkins file

--- myapp/hello.py
+++ myapp/hello.py
@@ -1,7 +1,20 @@
 import fire
+import requests 
+
+import fire
+import requests
 
 def hello(name="World"):
-  return "Hello %s!" % name
+    response = requests.get(f"https://jsonplaceholder.typicode.com/users?username={name}")
+
+    if response.status_code == 200:
+        user_data = response.json()
+        if user_data:
+            return f"Hello, {user_data[0]['name']}!"
+        else:
+            return f"Hello {name}! User not found in the API."
+    else:
+        return f"Hello {name}! Error while calling the API: {response.status_code}"
 
 if __name__ == '__main__':
-  fire.Fire(hello)
+    fire.Fire(hello)
